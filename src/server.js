5ebcc3ac082c4d7a3cd2f6f7f80b1b24d62b539e
Bashamega <adambashaahmednaji@gmail.com>
2024-09-28 17:27:46 +0300

    feat: get endpoint

--- src/server.js
+++ src/server.js
@@ -1,14 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+const { supabase } = require("./config/supabaseClient");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Define a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Define a route to fetch data from Supabase
+app.get("/timezones/:id", async (req, res) => {
+  const { id } = req.params;
+  let { data, error } = await supabase.from("timezones").select("*").eq('discord_user_id', id);
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.json(data);
 });
 
 // Start the server
